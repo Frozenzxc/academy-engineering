b8d053a81cfc94a28ff1c09fa9f45b935a014c4d
Frozenzxc <39233749+Frozenzxc@users.noreply.github.com>
2018-06-16 09:25:01 +0300

    added mobile menu

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
-console.log (4);
+
+$(document).ready(function() {
+  
+  $('body').on('click' , '.header__button', function() {
+
+   if (!$('.header__button').hasClass('clicked')) {
+    $('.navigation__list').addClass('navigation__list--toggle');
+    $('.header__mobile-btn').removeClass('header__mobile-btn').addClass('header__mobile-btn--off');
+    $('.header__button').addClass('clicked')
+   }
+   else {
+    $('.header__mobile-btn--off').removeClass('header__mobile-btn--off').addClass('header__mobile-btn');
+    $('.navigation__list').removeClass('navigation__list--toggle');
+    $('.header__button').removeClass('clicked')
+    }
+    }
+  );
+
+  $(window).resize(function() {		
+		if (  $(window).width > 800 ) {			
+			$('.navigation__list').addClass('navigation__list--inline');
+		 }
+	});
+});
+
+
+
